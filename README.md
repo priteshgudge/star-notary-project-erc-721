b6190fc18e7a373d4755bba4dfd2c8c14c569fc1
Pritesh Gudge <priteshgudge@gmail.com>
2020-06-22 23:36:18 +0530

    Contract Address added

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # star-notary-project-erc-721
 star-notary-project-erc-721
 
+## Notes
+Contract Name: PriteshToken
+Contract Symbol: PTC
+Rinkeby Contract Address: 0x8cFc72cDE833F46133E80A13F29ed5f788D39Cd1
+
 ### Versions
 Truffle v5.0.2 (core: 5.0.2)
 Solidity v0.5.0 (solc-js)
@@ -15,7 +20,15 @@ truffle develop
 > migrate --reset
 ```
 
-Web Application
+### Rinkeby
+```
+truffle migrate --reset --network rinkeby 
+Rinkeby Contract Address: 0x8cFc72cDE833F46133E80A13F29ed5f788D39Cd1
+```
+
+
+
+### Web Application
 ```
 npm run dev
 http://localhost:8080 in browser
--- truffle-config.js
+++ truffle-config.js
@@ -22,11 +22,11 @@
  *
  */
 
-// const HDWallet = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "25e8684586f04f738ce7a111833ae0fc";
 //
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   /**
@@ -51,7 +51,12 @@ module.exports = {
       port: 9545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
     },
-
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+        network_id: 4,       // rinkeby's id
+        gas: 4500000,        // rinkeby has a lower block limit than mainnet
+        gasPrice: 10000000000
+    }
     // Another network with more advanced options...
     // advanced: {
       // port: 8777,             // Custom port
